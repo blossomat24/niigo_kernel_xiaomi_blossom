54d668884cde11aa99f4ed1adfc1b66519a55ec9
Jonje <168655485+jonjeexe@users.noreply.github.com>
2026-05-30 12:13:39 +0530

    Increase zram disksize from 1500MB to 3072MB

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -1738,7 +1738,7 @@ static ssize_t disksize_store(struct device *dev,
 	struct zram *zram = dev_to_zram(dev);
 	int err;
 
-	disksize = (u64)1500 * SZ_1M;
+	disksize = (u64)3072 * SZ_1M;
 	if (!disksize)
 		return -EINVAL;
 
