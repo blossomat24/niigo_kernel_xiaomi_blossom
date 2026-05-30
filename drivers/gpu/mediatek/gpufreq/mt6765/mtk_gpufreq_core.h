11f1e429a8f773eeedb84d19fe8a48bdb55f901f
Jonje <168655485+jonjeexe@users.noreply.github.com>
2026-05-30 13:02:18 +0530

    Increase GPU frequency limits for battery protections

--- drivers/gpu/mediatek/gpufreq/mt6765/mtk_gpufreq_core.h
+++ drivers/gpu/mediatek/gpufreq/mt6765/mtk_gpufreq_core.h
@@ -148,21 +148,21 @@
  * Battery Over Current Protect
  **************************************************/
 #if IS_ENABLED(CONFIG_MTK_BATTERY_OC_POWER_THROTTLING)
-#define MT_GPUFREQ_BATT_OC_LIMIT_FREQ			(485000)/* KHz */
+#define MT_GPUFREQ_BATT_OC_LIMIT_FREQ			(500000)/* KHz */
 #endif
 
 /**************************************************
  * Battery Percentage Protect
  **************************************************/
 #if IS_ENABLED(CONFIG_MTK_BATTERY_PERCENTAGE_POWER_THROTTLING)
-#define MT_GPUFREQ_BATT_PERCENT_LIMIT_FREQ		(485000)/* KHz */
+#define MT_GPUFREQ_BATT_PERCENT_LIMIT_FREQ		(500000)/* KHz */
 #endif
 
 /**************************************************
  * Low Battery Volume Protect
  **************************************************/
 #if IS_ENABLED(CONFIG_MTK_LOW_BATTERY_POWER_THROTTLING)
-#define MT_GPUFREQ_LOW_BATT_VOLT_LIMIT_FREQ		(485000)/* KHz */
+#define MT_GPUFREQ_LOW_BATT_VOLT_LIMIT_FREQ		(500000)/* KHz */
 #endif
 
 /**************************************************
